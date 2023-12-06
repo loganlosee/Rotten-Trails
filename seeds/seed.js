9b7a1a1d5ab902b03a9b10ff80a3d463d6179cb7
Logan Losee <logan.losee99@gmail.com>
2023-12-05 21:26:58 -0700

    trailroutes withAuth and login post

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,23 @@
 // Example in a seed script (e.g., seed.js)
 
 const sequelize = require('../config/connection');
-const Trail = require('../models/Trail');
+const { User, Trail } = require('../models');
+
 const trailSeedData = require('./seed-trails');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   try {
     // Synchronize the model with the database
     await Trail.sync({ force: true });
 
-    // Create sample trails
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+      // Create sample trails
     await Trail.bulkCreate(trailSeedData);
 
     console.log('Seed data inserted successfully!');
@@ -19,7 +27,9 @@ const seedDatabase = async () => {
     // Close the database connection
     await sequelize.close();
   }
+  await sequelize.sync({ force: true });
+
+  process.exit(0);
 };
 
-// Call the seedDatabase function
 seedDatabase();
